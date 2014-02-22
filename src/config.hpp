859f0b59b34519ea527c57f185a2db871aa7a5be
shiluotang <shiluotang@sourceforge.net>
2014-02-22 23:22:15 +0800

    use more predefined macros for compilers.

--- src/config.hpp
+++ src/config.hpp
@@ -1,21 +1,12 @@
 #ifndef ODBCXX_MACROS_HPP_INCLUDED
 #define ODBCXX_MACROS_HPP_INCLUDED
 
-#define OS_UNIX 0
-#define OS_LINUX 1
-#define OS_WINDOWS 2
-
-#undef OS_TYPE
-
-#if defined (_WIN32) && defined(__MINGW32__) || defined(_WIN32) && defined(__MINGW64__) || defined(_WIN32) && defined(_MSC_VER)
-#define OS_TYPE OS_WINDOWS
-#else
-#define OS_TYPE OS_LINUX
-#endif
-
-#if OS_TYPE == OS_WINDOWS
-#include <windows.h>
-#else
+#if defined(_WIN32)
+#   include <windows.h>
+#elif defined(__CYGWIN__) || defined(__CYGWIN32__)
+#   include <windows.h>
+#elif defined(__MINGW__) || defined(__MINGW32__) || defined(__MINGW64__)
+#   include <windows.h>
 #endif
 
 #include <odbcinst.h>
