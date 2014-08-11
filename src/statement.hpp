d34a0301ac9a52eacb79cb2d079a3d98f5ccd44c
shiluotang <shiluotang@sourceforget.net>
2014-08-11 11:44:53 +0800

    add SQLGetData support

--- src/statement.hpp
+++ src/statement.hpp
@@ -133,12 +133,40 @@ namespace odbcxx {
 					SQLPOINTER target_value_ptr,
 					SQLLEN buf_len,
 					SQLLEN *len_or_indicator);
+
+			SQLRETURN get_data(SQLUSMALLINT, SQLSMALLINT, SQLPOINTER, SQLLEN, bool&);
+			SQLRETURN get_data(SQLUSMALLINT no, SQLSMALLINT &v, bool &is_null) {
+				return get_data(no, SQL_C_SSHORT, reinterpret_cast<SQLPOINTER>(&v), SQL_IS_SMALLINT, is_null);
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, SQLUSMALLINT &v, bool &is_null) {
+				return get_data(no, SQL_C_USHORT, reinterpret_cast<SQLPOINTER>(&v), SQL_IS_USMALLINT, is_null);
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, SQLINTEGER &v, bool &is_null) {
+				return get_data(no, SQL_C_SLONG, reinterpret_cast<SQLPOINTER>(&v), SQL_IS_INTEGER, is_null);
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, SQLUINTEGER &v, bool &is_null) {
+				return get_data(no, SQL_C_ULONG, reinterpret_cast<SQLPOINTER>(&v), SQL_IS_UINTEGER, is_null);
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, SQLREAL &v, bool &is_null) {
+				return get_data(no, SQL_C_FLOAT, reinterpret_cast<SQLPOINTER>(&v), SQL_IS_INTEGER, is_null);
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, SQLDOUBLE &v, bool &is_null) {
+				return get_data(no, SQL_C_DOUBLE, reinterpret_cast<SQLPOINTER>(&v), SQL_IS_INTEGER, is_null);
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, bool &v, bool &is_null) {
+				SQLCHAR c;
+				SQLRETURN retcode = get_data(no, SQL_C_BIT, reinterpret_cast<SQLPOINTER>(&c), 0, is_null);
+				v = (c != 0);
+				return retcode;
+			}
+			SQLRETURN get_data(SQLUSMALLINT no, std::string &v, bool &is_null);
+			SQLRETURN get_data(SQLUSMALLINT no, std::wstring &v, bool &is_null);
+
 		private:
 			friend class session;
 			friend class cursor;
 			friend class rowset;
 	};
-
 }
 
 #endif //ODBCXX_STATEMENT_HPP_INCLUDED
